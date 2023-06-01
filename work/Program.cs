99f2eac9fa55ed6cc42ab840dfb5536e6bb7c193
zhenshen <evgeniya.vea@gmail.com>
2023-06-01 21:29:08 +0300

    hw1

---
+++ work/Program.cs
@@ -0,0 +1,44 @@
+﻿Console.WriteLine("Введите число");
+int num1 = Convert.ToInt32( Console.ReadLine() );
+Console.WriteLine("Введите второе число");
+int num2 = Convert.ToInt32( Console.ReadLine() );
+if (num1 > num2)
+    Console.WriteLine("max = " + num1);
+else
+    Console.WriteLine("max = " + num2);
+
+Console.WriteLine("Введите 3 числа:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int max = num1;
+
+    if (num2 > max)
+    {
+    max = num2;
+    }
+
+    if (num3 > max)
+    {
+    max = num3;
+    }
+
+Console.WriteLine("Максимальное число - " + max);
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+    if (num % 2 == 1)
+Console.WriteLine("Число нечетное");
+    else 
+Console.WriteLine("Число четное");
+
+Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+int a = 1;
+    while (a <= n)
+    {
+    if (a % 2 == 0)
+        Console.Write(a + " ");
+    a++;
+    }
